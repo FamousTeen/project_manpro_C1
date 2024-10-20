b14c508b1df0d05d4c23a4647f8d68769d028576
ChynthiaOscar <136183139+ChynthiaOscar@users.noreply.github.com>
2024-10-20 15:37:44 +0700

    main_navbar

---
+++ resources/views/base/main_navbar.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main Navbar</title>
+    @vite('resources/css/app.css')
+</head>
+<!-- Colors: 
+        1. #740001 - merah gelap 
+        2. #ae0001 - merah terang 
+        3. #f6f1e3 - netral 
+        4. #002366 - biru terang 
+        5. #20252f - biru gelap 
+    -->
+<body class="bg-white">
+    <nav class="bg-[#20252f] text-white p-4">
+        <div class="container mx-auto flex justify-between items-center">
+            <!-- Logo Section -->
+            <a href="#" class="flex items-center">
+                <img src="../../../images/LOGO_MISDINAR.png" alt="Logo" class="h-12 w-12 mr-2 ml-4">
+                <span class="font-bold text-xl">MISDINAR ST. TARSISIUS GEREJA KATOLIK ROH KUDUS</span>
+            </a>
+
+            <!-- Navbar Buttons -->
+            <div class="flex space-x-8 items-center mr-4">
+                <a href="#" class="hover:text-[#ae0001] transition duration-300">Home</a>
+                <a href="#" class="hover:text-[#ae0001] transition duration-300">About Us</a>
+                <a href="#" class="hover:text-[#ae0001] transition duration-300">Contact</a>
+                <a href="#" class="bg-white text-[#20252f] py-2 px-6 rounded-full hover:bg-[#ae0001] hover:text-white transition duration-300">
+                    Login
+                </a>
+            </div>
+        </div>
+    </nav>
+</body>
+
+</html>
