8f91549634c473d9789abee544db07c7442f1a14
Valiant <c14220157@john.petra.ac.id>
2024-11-18 23:01:43 +0700

    seeder group and training

--- database/seeders/GroupSeeder.php
+++ database/seeders/GroupSeeder.php
@@ -20,5 +20,26 @@ class GroupSeeder extends Seeder
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
+
+        DB::table('groups')->insert([
+            'training_id' => 2,
+            'name' => "Kelompok 2",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('groups')->insert([
+            'training_id' => 3,
+            'name' => "Kelompok 3",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('groups')->insert([
+            'training_id' => 2,
+            'name' => "Kelompok 4",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
     }
 }
--- database/seeders/TrainingSeeder.php
+++ database/seeders/TrainingSeeder.php
@@ -24,5 +24,38 @@ class TrainingSeeder extends Seeder
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
+
+        DB::table('trainings')->insert([
+            'admin_id' => 1,
+            'training_date' => Carbon::now()->addDays(3),
+            'place' => 'Bugis',
+            'contact_person' => 'Marcella',
+            'phone_number' => '081929302947',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce metus erat, feugiat eu hendrerit aliquet, pulvinar eu urna. Curabitur eu sagittis diam. Maecenas faucibus scelerisque.',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('trainings')->insert([
+            'admin_id' => 1,
+            'training_date' => Carbon::now()->addDays(4),
+            'place' => 'Wakasek',
+            'contact_person' => 'Marimar',
+            'phone_number' => '081929302947',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce metus erat, feugiat eu hendrerit aliquet, pulvinar eu urna. Curabitur eu sagittis diam. Maecenas faucibus scelerisque.',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('trainings')->insert([
+            'admin_id' => 1,
+            'training_date' => Carbon::now()->addDays(2),
+            'place' => 'Lab. Komputer',
+            'contact_person' => 'Batagor',
+            'phone_number' => '081929302947',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce metus erat, feugiat eu hendrerit aliquet, pulvinar eu urna. Curabitur eu sagittis diam. Maecenas faucibus scelerisque.',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
     }
 }
