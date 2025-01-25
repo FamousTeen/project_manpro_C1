e14880e5b076f3126a7d6660269d34ac57d149c9
charissaa <c14220188@john.petra.ac.id>
2025-01-25 15:40:32 +0700

    Update jadwal_misa.blade.php

--- resources/views/admin/jadwal_misa.blade.php
+++ resources/views/admin/jadwal_misa.blade.php
@@ -8,7 +8,7 @@ $accounts = App\Models\Account::all();
 
 <header class="mt-16 p-8">
     <h1 class="text-2xl font-semibold text-[#20252f]">JADWAL MISA</h1>
-    <div class="flex space-x-4 mt-4 px-4 overflow-x-auto whitespace-nowrap sm:justify-center sm:overflow-hidden">
+    <div class="flex space-x-4 mt-4 px-4 overflow-x-auto whitespace-nowrap sm:justify-center lg:overflow-hidden">
         <!-- Status Filter Buttons -->
         <button class="px-4 py-2 rounded-lg bg-[#740001] text-white status-filter-button" data-status="all">Semua</button>
         <button class="px-4 py-2 bg-[#f6f1e3] rounded-lg hover:bg-[#740001] hover:text-white status-filter-button" data-status="Proses">Proses</button>
