751bba38efde0c654894ac35bea936d880a92083
Valiant <c14220157@john.petra.ac.id>
2025-01-26 14:44:35 +0700

    Update daftar_pelatihan.blade.php

--- resources/views/admin/training/daftar_pelatihan.blade.php
+++ resources/views/admin/training/daftar_pelatihan.blade.php
@@ -34,6 +34,9 @@
             @foreach ($trainings as $training)
                 @if ($training->group->count() > 0)
                     @foreach ($training->group as $g)
+                        @php
+                            dd($g);
+                        @endphp
                         <div class="my-6 rounded-xl py-6 px-6 flex flex-col lg:flex-row bg-[#f6f1e3]">
                             <div class="flex flex-col lg:flex-grow">
                                 <p class="font-semibold text-lg lg:text-xl">
