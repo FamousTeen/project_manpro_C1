2f05780eddea0ab1c36f8bf0b0d0461a08482598
Valiant <c14220157@john.petra.ac.id>
2025-01-26 15:01:42 +0700

    Update daftar_pelatihan.blade.php

--- resources/views/admin/training/daftar_pelatihan.blade.php
+++ resources/views/admin/training/daftar_pelatihan.blade.php
@@ -35,7 +35,7 @@
                     $training_details = $training->trainingDetails;
                 @endphp
                 @if ($training_details->count() > 0)
-                    @foreach ($training_details->groups as $g)
+                    @foreach ($training_details as $td)
                         <div class="my-6 rounded-xl py-6 px-6 flex flex-col lg:flex-row bg-[#f6f1e3]">
                             <div class="flex flex-col lg:flex-grow">
                                 <p class="font-semibold text-lg lg:text-xl">
@@ -44,7 +44,7 @@
                                 <p class="text-sm lg:text-base">
                                     {{ Carbon::parse($training->training_date)->translatedFormat('H.i') }} WIB
                                 </p>
-                                <h1 class="text-xl lg:text-3xl mt-4 mb-4">{{ $g->name }}</h1>
+                                <h1 class="text-xl lg:text-3xl mt-4 mb-4">{{ $td->groups->name }}</h1>
                                 <p class="text-sm lg:text-base">Contact Person : {{ $training->contact_person }} ( <a
                                         style="color: blue; text-decoration: underline; cursor: pointer;"
                                         href="https://wa.me/62{{ $training->phone_number }}">{{ $training->phone_number }}</a>)
@@ -53,7 +53,7 @@
                             </div>
                             <!-- Button group for actions -->
                             <div class="flex flex-col lg:flex-row lg:items-end lg:justify-end lg:space-x-2 mt-4 lg:mt-0">
-                                <a href="{{ route('trainings.edit', [$training, $g]) }}"
+                                <a href="{{ route('trainings.edit', [$training, $td->groups]) }}"
                                     class="px-6 py-2 bg-[#002366] hover:bg-[#20252f] text-white rounded-lg text-center mb-2 lg:mb-0 lg:mr-2">
                                     Edit
                                 </a>
